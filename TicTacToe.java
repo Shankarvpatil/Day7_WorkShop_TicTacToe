a05b121e4b95f11c2b9717ab60f6eba184a065bf
shankar <shankarvpatil1599@gmail.com>
2021-07-01 20:25:10 +0530

    Show Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,14 +8,15 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		getBoard();
 		getPlayer();
+		currentBoard();
 	}
-
+	
 	 static void getBoard() {
 		 for (int i = 0; i < 10; i++) {
-			 board[i] = '-';
+			 board[i] = ' ';
 		 }
 	 }
-
+	 
 	 static void getPlayer() {
 		 System.out.print("Choose an Option x or o : ");
 		 player = scanner.next().charAt(0);
@@ -24,5 +25,22 @@ public class TicTacToeGame {
 		 else
 			 computer ='x';
 		 System.out.println("You Selected : " +player);
+	 }
+	
+	 static void showBoard() {
+	        System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
+	        System.out.println(".....|......|.....");
+	        System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
+	        System.out.println(".....|......|.....");
+	        System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
+	 }
+	 private static void currentBoard() {
+	        int RADIX = 10;
+	        System.out.println("\n");
+	        for( int i=1; i<10; i++) {
+	            if (board[i] !='x' && board[i] !='o')
+	                board[i] = Character.forDigit(i, RADIX);
+	        }
+	        showBoard();
 	    }
 }
