f7a9e737d64fdb3ecfbea914a1a0a5f782dcfb71
shankar <shankarvpatil1599@gmail.com>
2021-07-01 20:18:48 +0530

    Players to Choose

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,28 @@
-package com.bridgelabz.workshop.tictactoe;
+import java.util.Scanner;
 
 public class TicTacToeGame {
 	static char[] board = new char[10];
+	static char player,computer;
+	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe");
-
+		getBoard();
+		getPlayer();
 	}
-	static void getBoard() {
-        for(int i=0; i<10; i++) {
-            board[i]=' ';
-        }
-    }
+
+	 static void getBoard() {
+		 for (int i = 0; i < 10; i++) {
+			 board[i] = '-';
+		 }
+	 }
+
+	 static void getPlayer() {
+		 System.out.print("Choose an Option x or o : ");
+		 player = scanner.next().charAt(0);
+		 if (player == 'x')
+			 computer='o';
+		 else
+			 computer ='x';
+		 System.out.println("You Selected : " +player);
+	    }
 }
